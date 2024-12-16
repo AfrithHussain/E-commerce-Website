ef795cfce07b0ac47850bbf861ad5e7b956348d5
AfrithHussain <afrithhussain64@gmail.com>
2024-12-16 15:47:57 +0530

    link updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Heromodel from '../assets/heromodel.jpg';
 
 function Hero() {
@@ -12,8 +13,9 @@ function Hero() {
             <hr className=' w-0 md:w-20 xl:w-24 h-[3px] bg-neutral-700'/>
           </div>
           <p className='outfit  text-sm  md:text-xl leading-7 md:leading-9  md:text-center lg:text-start xl:mx-20'>Discover the latest trends, timeless classics, and exclusive collections — all in one place. Your fashion journey starts here.</p>
-          <button className='bg-black text-white py-3 px-6 md:px-8 rounded-sm outfit hover:bg-neutral-800 transition duration-200'>Buy Now</button>
-        </div>
+          
+          <Link to={'/collection'}> <button className='bg-black text-white py-3 px-6 md:px-8 rounded-sm outfit hover:bg-neutral-800 transition duration-200'>Buy Now</button></Link>
+         </div>
 
         {/* div 2 */}
         <div className="w-full lg:w-[1000px] flex justify-center md:justify-start mt-10 md:mt-0">
